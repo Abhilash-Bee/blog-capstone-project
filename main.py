86155ba9e779b4e07f6c8a3144ef1c0f4e398828
Abhilash-Bee <abhilashjanarrdhan@gmail.com>
2022-09-22 22:11:29 +0530

    Back to sqlite

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def load_user(user_id):
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
